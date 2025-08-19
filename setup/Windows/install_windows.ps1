84a4d878dd7abbc4721067f1a89fd123392ba131
Myrenic <mike@tuntelder.com>
2025-08-19 16:10:52 +0200

    update extensions.json, install_windows.ps1 and update_windows.ps1

--- setup/Windows/install_windows.ps1
+++ setup/Windows/install_windows.ps1
@@ -19,5 +19,10 @@ $symlinks = @(
 # Call the helper function
 New-Symlinks -Links $symlinks
 
+code --list-extensions > extensions.txt
+code --list-extensions | ForEach-Object { code --uninstall-extension $_ }
+Get-Content extensions.txt | ForEach-Object { code --install-extension $_ }
+remove-item extensions.txt
+
 wsl --install
 Restart-Computer -Force
